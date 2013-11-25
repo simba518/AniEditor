25df5ec8f5e12fced28270005f10d4454c75d92e
= <=>
2013-11-25 19:45:18 +0800

    success to write the ui test main file for windows.

--- SRC/UI/main_UITest.cpp
+++ SRC/UI/main_UITest.cpp
@@ -5,22 +5,31 @@
 #include "AniEditMainWin.h"
 using namespace UTILITY;
 
+#ifdef WIN32
+const string TEST_UI_INIF= "D:\\lsw\\AnimationEditor\\TestCase\\TestData\\ani_ui_test_windows.ini";
+#else
+const string TEST_UI_INIF= "/home/simba/Workspace/AnimationEditor/TestCase/TestData/ani_ui_test.ini";
+#endif
+
 int main(int argc, char *argv[]){
 
-	QApplication application(argc,argv);
-	QMainWindow w;
-	ANI_EDIT_UI::AniEditMainWin m_mainw("",&w);
+  QApplication application(argc,argv);
+  QMainWindow w;
+  ANI_EDIT_UI::AniEditMainWin m_mainw("",&w);
 
-	// auto record and replay
-	pQInputEventRecorderCmd recorder;
-	if(argc >= 3){ 
-		recorder = pQInputEventRecorderCmd(new QInputEventRecorderCmd(&w));
-		if (3 == argc)
-			recorder->setCmd(argv[argc-1]);
-		else if(4 == argc)
-			recorder->setCmd(argv[argc-2],argv[argc-1]);
-	}
+  // auto record and replay
+  pQInputEventRecorderCmd recorder;
+  if(argc >= 2){
+	recorder = pQInputEventRecorderCmd(new QInputEventRecorderCmd(&w));
+	if (2 == argc)
+	  recorder->setCmd(argv[argc-1]);
+	else if(2 == argc)
+	  recorder->setCmd(argv[argc-2],argv[argc-1]);
+  }
 
-	m_mainw.show();
-	return  application.exec();
+  if(!m_mainw.loadInitFile(TEST_UI_INIF))
+	ERROR_LOG("failed to load the init file: "<<TEST_UI_INIF);
+
+  m_mainw.show();
+  return  application.exec();
 }
